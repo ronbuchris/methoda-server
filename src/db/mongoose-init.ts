0303aa6083d95d427f2614b1a0919d52bc3961e0
Ron Buchris <ronbuchris@gmail.com>
2023-03-04 21:36:49 +0200

    change mongo to env

--- src/db/mongoose-init.ts
+++ src/db/mongoose-init.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb+srv://ronbuchris:9qqpDhqZOJxmZqzS@cluster0.8otc2.mongodb.net/methoda', { retryWrites: true, w: 'majority' })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.8otc2.mongodb.net/methoda`, { retryWrites: true, w: 'majority' })
 
 mongoose.connection.on('connected', function () {
     console.log('Mongoose conected')
